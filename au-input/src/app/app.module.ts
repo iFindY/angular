fb4a3869d00aa6d6ba0f32d8ed457b5e1dd677e1
Arkadi <arkadi.daschkewitsch@gmail.com>
2019-12-02 21:15:57 +0100

    modul structur

--- au-input/src/app/app.module.ts
+++ au-input/src/app/app.module.ts
@@ -2,23 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
-import {HttpClientModule} from "@angular/common/http";
-import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
-import { InputRefDirective } from './lib/common/input-ref.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { AuInputModule } from './lib/au-input.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuFaInputComponent,
-    InputRefDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AuInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
