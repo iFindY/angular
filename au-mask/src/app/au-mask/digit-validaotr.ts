b9d175a4e59258322e89c31c44ef5c9e2c093eee
Arkadi <arkadi.daschkewitsch@gmail.com>
2019-12-08 12:29:27 +0100

    delet backspace events

---
+++ au-mask/src/app/au-mask/digit-validaotr.ts
@@ -0,0 +1,18 @@
+type DigitValidator = (char: string) => boolean;
+
+const anyValidator = char => true;
+const numericValidator = char => /[0-9]/.test(char);
+const lowerCaseValidator = char => /[a-z]/.test(char);
+const upperCaseValidator = char => /[A-Z]/.test(char);
+const numberRangeValidator = (maxValue: number, char: string) => numericValidator(char) && parseInt(char) <= maxValue;
+
+export const neverValidator = char => false;
+export const maskDigitValidators: { [key: string]: DigitValidator } = {
+  'a': lowerCaseValidator,
+  'A': upperCaseValidator,
+  '*': anyValidator
+};
+
+for (let i = 0; i <= 9; i++) {
+  maskDigitValidators[i] = numberRangeValidator.bind(undefined, i);
+}
--- au-mask/src/app/au-mask/mask.directive.ts
+++ au-mask/src/app/au-mask/mask.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
-import { LEFT_ARROW, overrideCharAtPosition, RIGHT_ARROW, SPECIAL_CHARACTERS, TAB } from './mask.utils';
+import { BACKSPACE, DELETE, LEFT_ARROW, overrideCharAtPosition, RIGHT_ARROW, SPECIAL_CHARACTERS, TAB } from './mask.utils';
 import { el } from '@angular/platform-browser/testing/src/browser_util';
+import { maskDigitValidators, neverValidator } from './digit-validaotr';
 
 @Directive({
   selector: '[mask]'
@@ -26,7 +27,6 @@ export class MaskDirective implements OnInit {
 
     const key = String.fromCharCode(keyCode),
       cursorPosition = this.input.selectionStart;
-    let nextCurPos: number;
 
     switch (keyCode) {
       case LEFT_ARROW:
@@ -35,13 +35,70 @@ export class MaskDirective implements OnInit {
       case RIGHT_ARROW:
         this.handelRightArrow(cursorPosition);
         break;
+      case BACKSPACE:
+        this.handelBackSpace(cursorPosition);
+        break;
+      case DELETE:
+        this.handelDelete(cursorPosition);
+        break;
     }
 
-    overrideCharAtPosition(this.input, cursorPosition, key);
-    this.handelRightArrow(cursorPosition);
+    const maskDigit = this.mask.charAt(cursorPosition),
+      digitValidator = maskDigitValidators[maskDigit] || neverValidator;
+
+    if (digitValidator((key))) {
+      overrideCharAtPosition(this.input, cursorPosition, key);
+      this.handelRightArrow(cursorPosition);
+    }
+  }
+
+  handelBackSpace(cursorPosition) {
+    let nextCurPos = this.getTextLeftCurPosition(cursorPosition);
+
+    if (nextCurPos != undefined) {
+      overrideCharAtPosition(this.input, nextCurPos, '_');
+      this.input.setSelectionRange(nextCurPos, nextCurPos);
+    }
+  }
+
+  handelDelete(cursorPosition) {
+    let nextCurPos = this.getNextRightCurPosition(cursorPosition);
+
+    if (cursorPosition != undefined && this.input.value[cursorPosition] !== '_') {
+      overrideCharAtPosition(this.input, cursorPosition, '_');
+      this.input.setSelectionRange(cursorPosition, cursorPosition);
+    } else if (nextCurPos != undefined && this.input.value[cursorPosition] === '_') {
+      overrideCharAtPosition(this.input, nextCurPos, '_');
+      this.input.setSelectionRange(nextCurPos, nextCurPos);
+
+    }
   }
 
   handelLeftArrow(cursorPosition) {
+    let nextCurPos = this.getTextLeftCurPosition(cursorPosition);
+
+    if (nextCurPos != undefined) this.input.setSelectionRange(nextCurPos, nextCurPos);
+  }
+
+  handelRightArrow(cursorPosition) {
+    let nextCurPos = this.getNextRightCurPosition(cursorPosition);
+
+    if (nextCurPos != undefined) this.input.setSelectionRange(nextCurPos, nextCurPos);
+  }
+
+  private getNextRightCurPosition(cursorPosition) {
+    let nextCurPos: number;
+    this.input.value
+      .slice(cursorPosition + 1)
+      .split('')
+      .reduce((index, char) => {
+        if (!nextCurPos && !SPECIAL_CHARACTERS.includes(char)) nextCurPos = cursorPosition + index;
+        return index += +1;
+      }, 1);
+    return nextCurPos;
+  }
+
+  private getTextLeftCurPosition(cursorPosition): number {
     let nextCurPos: number;
     const previousCurPos: string[] =
       this.input.value
@@ -53,21 +110,7 @@ export class MaskDirective implements OnInit {
         if (!nextCurPos && !SPECIAL_CHARACTERS.includes(char)) nextCurPos = index;
         return index += -1;
       }, previousCurPos.length - 1);
-
-    if (nextCurPos != undefined) this.input.setSelectionRange(nextCurPos, nextCurPos);
-  }
-
-  handelRightArrow(cursorPosition) {
-    let nextCurPos: number;
-    this.input.value
-      .slice(cursorPosition + 1)
-      .split('')
-      .reduce((index, char) => {
-        if (!nextCurPos && !SPECIAL_CHARACTERS.includes(char)) nextCurPos = cursorPosition + index;
-        return index += +1;
-      }, 1);
-
-    if (nextCurPos != undefined) this.input.setSelectionRange(nextCurPos, nextCurPos);
+    return nextCurPos;
   }
 
   buildPlaceHolder(): string {
